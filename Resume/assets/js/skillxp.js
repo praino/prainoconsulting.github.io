83f1f5a42df6ac1dc54d8109e3a7531fa2d20cea
praino <53439431+praino@users.noreply.github.com>
2019-07-29 13:59:35 -0700

    Add files via upload

---
+++ Resume/assets/js/skillxp.js
@@ -0,0 +1,77 @@
+(function($) {
+    'use strict';
+
+    $('.1').rProgressbar({
+        percentage: 99,
+        fillBackgroundColor: '#009688'
+        , ShowProgressCount: false
+    });
+
+    $('.2').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#4caf50'
+        , ShowProgressCount: false
+    });
+
+
+    $('.3').rProgressbar({
+        percentage: 92,
+        fillBackgroundColor: '#607d8b'
+        , ShowProgressCount: false
+    });
+
+    $('.4').rProgressbar({
+        percentage: 85,
+        fillBackgroundColor: '#03a9f4'
+        , ShowProgressCount: false
+    });
+
+    $('.5').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#9c27b0'
+        , ShowProgressCount: false
+    });
+
+    $('.6').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#009688'
+        , ShowProgressCount: false
+    });
+
+    $('.7').rProgressbar({
+        percentage: 77,
+        fillBackgroundColor: '#4caf50'
+        , ShowProgressCount: false
+    });
+
+
+    $('.8').rProgressbar({
+        percentage: 77,
+        fillBackgroundColor: '#607d8b'
+        , ShowProgressCount: false
+    });
+
+    $('.9').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#03a9f4'
+        , ShowProgressCount: false
+    });
+
+    $('.10').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#9c27b0'
+        , ShowProgressCount: false
+    });
+    $('.11').rProgressbar({
+        percentage: 70,
+        fillBackgroundColor: '#009688'
+        , ShowProgressCount: false
+    });
+
+    $('.12').rProgressbar({
+        percentage: 70,
+        fillBackgroundColor: '#4caf50'
+        , ShowProgressCount: false
+    });
+
+})(jQuery);
